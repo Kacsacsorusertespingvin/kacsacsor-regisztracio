57801c7ac877583d7c387f4fd12bff5d064d6210
Kacsacsorusertespingvin <szabomedveb@gmail.com>
2025-06-12 19:44:16 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,21 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDNfbKRe9IBAnTKHoSGj06dWULk3i_kag",
+  authDomain: "segitsegkozpont.firebaseapp.com",
+  projectId: "segitsegkozpont",
+  storageBucket: "segitsegkozpont.firebasestorage.app",
+  messagingSenderId: "910810037566",
+  appId: "1:910810037566:web:3022ce930951c5d61b406f",
+  measurementId: "G-T6PQ7DNVHS"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
 
